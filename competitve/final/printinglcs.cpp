fa711fd9651f32c8b39ab4c7b709078900dcfb1e
Adarsh kandwal <41911887+AKAY07@users.noreply.github.com>
2020-06-03 17:26:41 +0530

    Add files via upload

---
+++ competitve/final/printinglcs.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+string s1,s2;cin>>s1>>s2;
+int l1=s1.length(),l2=s2.length();
+vector<vector<int>>t(l1+1,vector<int>(l2+1));
+for(int i=0;i<l1+1;i++){
+for(int j=0;j<l2+1;j++)if(i==0||j==0)t[i][j]=0;}
+
+for(int i=1;i<l1+1;i++){
+for(int j=1;j<l2+1;j++){
+ if(s1[i-1]==s2[j-1]) t[i][j]=1+t[i-1][j-1];
+ else t[i][j]=max(t[i-1][j],t[i][j-1]);
+}
+}
+
+for(int i=0;i<l1+1;i++){
+for(int j=0;j<l2+1;j++)cout<<t[i][j]<<" ";
+    cout<<endl;
+}
+int i=l1+1,j=l2+1;
+string result;
+while(i>0&&j>0){
+    if(s1[i-1]==s2[j-1])result.push_back(s1[i-1]),i--,j--;
+    else{ 
+    if(t[i-1][j]>t[i][j-1])i--;
+    else j--;
+    }
+}
+reverse(result.begin(),result.end());
+cout<<result;
+
+return 0;
+}
