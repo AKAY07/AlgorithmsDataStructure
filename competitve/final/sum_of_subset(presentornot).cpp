f0936a0e1f6084ed39c02f13d048d464d3b3acc0
Adarsh kandwal <41911887+AKAY07@users.noreply.github.com>
2020-06-03 17:26:07 +0530

    Add files via upload

--- competitve/final/sum_of_subset(presentornot).cpp
+++ competitve/final/sum_of_subset(presentornot).cpp
@@ -1,38 +1,38 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+    #include<iostream>
+    #include<vector>
+    using namespace std;
 
-int main(){
- int n,sum;
- cin>>n;
- cin>>sum;
- vector<int>arr(n);
- for(int i=0;i<n;i++)cin>>arr[i];
- 
- vector<vector<int>>t(n+1,vector<int>(sum+1));  
- //intilizing
- for(int i=0;i<n+1;i++){
-     for(int j=0;j<sum+1;j++){
-         if(i==0)t[i][j]=0;
-         if(j==0)t[i][j]=1;}}
-//logic
-for(int i=1;i<n+1;i++){
-    for(int j=1;j<sum+1;j++){
-        if(arr[i-1]<=j)
-        t[i][j]=(t[i-1][j-arr[i-1]])||(t[i-1][j]);
-        else 
-        t[i][j]=t[i-1][j];
-        
-       
+    int main(){
+     int n,sum;
+     cin>>n;
+     cin>>sum;
+     vector<int>arr(n);
+     for(int i=0;i<n;i++)cin>>arr[i];
+     
+     vector<vector<int>>t(n+1,vector<int>(sum+1));  
+     //intilizing
+     for(int i=0;i<n+1;i++){
+         for(int j=0;j<sum+1;j++){
+             if(i==0)t[i][j]=0;
+             if(j==0)t[i][j]=1;}}
+    //logic
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<sum+1;j++){
+            if(arr[i-1]<=j)
+            t[i][j]=(t[i-1][j-arr[i-1]])||(t[i-1][j]);
+            else 
+            t[i][j]=t[i-1][j];
+            
+           
+        }
     }
-}
- for(int i=0;i<n+1;i++){
-     for(int j=0;j<sum+1;j++){
-         if(i==0)t[i][j]=0;
-         if(j==0)t[i][j]=1;
-         cout<<t[i][j]<<" ";
+     for(int i=0;i<n+1;i++){
+         for(int j=0;j<sum+1;j++){
+             if(i==0)t[i][j]=0;
+             if(j==0)t[i][j]=1;
+             cout<<t[i][j]<<" ";
+         }
+         cout<<endl;
      }
-     cout<<endl;
- }
-return 0;
-}
+    return 0;
+    }
