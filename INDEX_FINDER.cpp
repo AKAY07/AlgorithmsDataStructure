8f8b2d8621dd6f2a2ff3d7416c1b81b289860318
Adarsh kandwal <41911887+AKAY07@users.noreply.github.com>
2020-06-06 22:14:57 +0530

    Create INDEX_FINDER.cpp

---
+++ INDEX_FINDER.cpp
@@ -0,0 +1,32 @@
+int minIndex(int arr[], int s, int e) 
+{ 
+    int sml = INT32_MAX; 
+    int mindex; 
+    for (int i = s; i < e; i++) 
+    { 
+        if (sml > arr[i]) 
+        { 
+            sml = arr[i]; 
+            mindex = i; 
+        } 
+    } 
+    return mindex; 
+} 
+  
+void fun2(int arr[], int start_index, int end_index) 
+{ 
+  if(start_index >= end_index)    
+     return; 
+  int min_index;  
+  int temp;  
+  
+  /*minIndex() returns index of minimum value in  
+    array arr[start_index...end_index] */
+  min_index = minIndex(arr, start_index, end_index); 
+  
+  temp = arr[start_index]; 
+  arr[start_index] = arr[min_index]; 
+  arr[min_index] = temp;         
+  
+  fun2(arr, start_index + 1, end_index); 
+}  
